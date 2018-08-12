9ce09b8aa929d95eceee20255b9008f7d57f736e
Bentley <bentleygtr69@yahoo.com>
2018-08-11 19:46:37 -0500

    working...enough

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsList = [
+    {
+        name: "Nathan Explosion",
+        photo: "https://i.kym-cdn.com/entries/icons/original/000/016/925/498ed76be651cffb6bb9bac6a9bb75c3.png",
+        preferences: [5, 3, 2, 4, 2, 1, 3, 4, 2, 1]
+    },
+    {
+        name: "Eric Wareheim",
+        photo: "https://vignette.wikia.nocookie.net/creepypasta/images/2/2d/WMdf96FnSVOjW3qnFXHG_eric_wareheim.jpg/revision/latest?cb=20140413111441",
+        preferences: [5, 2, 1, 1, 1, 4, 5, 4, 3, 2]
+
+    },
+    {
+        name: "Steve Brule",
+        photo: "https://cdn.vox-cdn.com/thumbor/MdVz6b_Wzdj07pOkyW7EBSmESvA=/0x0:1620x1080/920x613/filters:focal(0x0:1620x1080):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/45508410/brule-wallpaper.0.0.jpg",
+        preferences: [5, 1, 5, 5, 3, 2, 5, 4, 3, 5]
+    }
+];
+
+module.exports = friendsList;
--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports = function(app){
 // HTML : Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 };
